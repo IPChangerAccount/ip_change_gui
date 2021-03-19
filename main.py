ff697a8a845d2185b44b3f59b7181cf7f5f5954c
Liciliy <vladyslav.daniliants@gmail.com>
2021-03-19 14:09:38 +0200

    Cosmetics.

--- main.py
+++ main.py
@@ -45,16 +45,18 @@ class State:
 def change_ip(program_state : State):
 
     try:
+        print('\n\nStarting get new IP procedure...')
         conn_details = NetworkHandler.get_connection_details()
 
-
         api_request = ApiRequest(conn_details[NetworkHandler.DEFAULT_GATEWAY])
-
+        print('    Requesting new gateway...')
         new_gate = api_request.get_new_gate()
+        print('    New gateway {new_gate} received. Applying...')
+        
 
         if NetworkHandler.change_default_gateway(new_gate, conn_details):
             api_request.confirm_gate_usage_towards_api(new_gate)
-            print('Success')
+            print('Success! IP changed.')
         else:
             # TODO Here send failed result to the gateway
             print('Failed')
